443cd0127241a82e12416f2a3bb199cd4a81e76e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-25 11:22:16 -0400

    Add the RocketsContent component and Import the Rocket.css file

--- src/views/Rockets.jsx
+++ src/views/Rockets.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllRockets } from '../redux/rockets/rocketsSlice';
 import Layout from './Layout';
+import RocketsContent from '../components/RocketsContent';
+import '../assets/styles/Rocket.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -12,7 +14,11 @@ const Rockets = () => {
 
   return (
     <Layout>
-      <section id="rockets" />
+      <section id="rockets">
+        <div className="container">
+          <RocketsContent />
+        </div>
+      </section>
     </Layout>
   );
 };
