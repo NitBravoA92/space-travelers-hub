24f6973e0dcc184b16eedc0b6f8990362f6a5b80
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-25 17:54:17 -0400

    Add the JoinedMissions components in My Profile view component

--- src/views/MyProfile.jsx
+++ src/views/MyProfile.jsx
@@ -1,4 +1,5 @@
 import Layout from './Layout';
+import JoinedMissions from '../components/JoinedMissions';
 import ReservedRockets from '../components/ReservedRockets';
 import '../assets/styles/MyProfile.css';
 
@@ -7,6 +8,7 @@ const MyProfile = () => (
     <section id="myProfile">
       <div className="container">
         <div className="services-content">
+          <JoinedMissions />
           <ReservedRockets />
         </div>
       </div>
