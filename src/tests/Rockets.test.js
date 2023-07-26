e954ecf07581b472e45f0d65d6f6c56071556630
Kidd254 <lorenkioko@gmail.com>
2023-07-26 22:26:54 +0300

    Create the test cases for the Rockets view component

---
+++ src/tests/Rockets.test.js
@@ -0,0 +1,48 @@
+import "@testing-library/jest-dom/extend-expect";
+import { screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+import Rockets from '../views/Rockets';
+import { renderWithProviders } from "../helpers/helper-for-test";
+
+describe("The Rockets component", () => {
+  test("renders into the DOM correctly", () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <Rockets />
+      </MemoryRouter>,
+    );
+
+    const renderRocketsPageContainer = document.querySelector("#rockets");
+    expect(renderRocketsPageContainer).toBeInTheDocument();
+  });
+
+  test("should render the navigation menu with the 'Rockets', 'Missions' and 'My Profile' Links", () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <Rockets />
+      </MemoryRouter>,
+    );
+
+    const navigationMenu = document.querySelector("nav");
+    const rocketsLink = screen.getByText(/Rockets/i);
+    const missionsLink = screen.getByText(/Missions/i);
+    const myProfileLink = screen.getByText(/My Profile/i);
+
+    expect(navigationMenu).toBeInTheDocument();
+    expect(rocketsLink).toBeInTheDocument();
+    expect(missionsLink).toBeInTheDocument();
+    expect(myProfileLink).toBeInTheDocument();
+  });
+
+  test("renders the rockets main content", () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <Rockets />
+      </MemoryRouter>,
+    );
+
+    const renderRocketsContent = document.querySelector(".rockets-list");
+    expect(renderRocketsContent).toBeInTheDocument();
+  });
+
+});
