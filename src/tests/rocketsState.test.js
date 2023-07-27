a18b80e1e1db47e49b43291304bc01410a52248f
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-27 00:51:39 -0400

    Create test cases for the Rockets state

---
+++ src/tests/rocketsState.test.js
@@ -0,0 +1,18 @@
+import store from "../redux/store";
+import { getAllRockets } from "../redux/rockets/rocketsSlice";
+
+describe("The Rockets state", () => {
+  test("must be an object with an empty array initially", async () => {
+    const initState = await store.getState().rockets;
+
+    expect(initState).toEqual({ rockets: [], isLoading: false, error: null });
+  });
+
+  test("should be updated with the Rockets data when the 'getAllRockets' action is dispatched", async () => {
+    const updateRocketsState = await store.dispatch(getAllRockets());
+    const newState = await store.getState().rockets;
+
+    expect(updateRocketsState.type).toEqual("rockets/getAllRockets/fulfilled");
+    expect(newState.rockets).toBeTruthy();
+  });
+});
