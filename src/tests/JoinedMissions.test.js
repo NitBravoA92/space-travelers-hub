f5f069d6c93c104b3746016d6d9b4a9acaa3170e
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-26 22:21:44 -0400

    Create the test cases to validate the JoinedMissions component is rendering

---
+++ src/tests/JoinedMissions.test.js
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom/extend-expect";
+import JoinedMissions from '../components/JoinedMissions';
+import { renderWithProviders } from "../helpers/helper-for-test";
+
+describe("The JoinedMissions component", () => {
+  test("renders into the DOM", () => {
+    const {tree } =  renderWithProviders(<JoinedMissions />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("renders an empty <ul> element when user does not have any Missions joined", () => {
+    renderWithProviders(<JoinedMissions />);
+
+    const missionsJoined = document.querySelectorAll("li");
+
+    expect(missionsJoined).toHaveLength(0);
+  });
+
+});
